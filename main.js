67fab734e5cd99788ae14138744a8246280b6dc1
VarshaJain1 <vjain1812002@gmail.com>
2025-07-23 20:23:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 
+# Revenue = Quantity Sold × Price per Unit
+
+# Input values
+quantity_sold = 120  # you can change this value
+price_per_unit = 15.50  # you can change this too
+
+# Calculate revenue
+revenue = quantity_sold * price_per_unit
+
+# Display result
+print("Total Revenue is:", revenue)
